6a239514f6f7fb7bc46a13fb6d132f5ba39da667
Muaz Khan <muazkh@gmail.com>
2014-11-18 09:44:39 +0500

    ConcatenateBlobs-v1.0.1 updated. via #1

--- ConcatenateBlobs.js
+++ ConcatenateBlobs.js
@@ -1,4 +1,4 @@
-// Last time updated at Sep 15, 2014, 08:32:23
+// Last time updated at Nov 18, 2014, 08:32:23
 
 // Latest file can be found here: https://cdn.webrtc-experiment.com/ConcatenateBlobs.js
 
@@ -38,17 +38,30 @@
             var byteLength = 0;
             buffers.forEach(function(buffer) {
                 byteLength += buffer.byteLength;
+                // TODO(datermine): Figure out if this is necessary.
+                if (buffer.byteLength % 2 != 0) {
+                    // byteLength--;
+                }
             });
             var tmp = new Uint16Array(byteLength);
 
             var lastOffset = 0;
             buffers.forEach(function(buffer) {
                 // BYTES_PER_ELEMENT == 2 for Uint16Array
-                if (buffer.byteLength % 2 != 0) {
-                    delete buffer[byteLength - 1];
+                var bufferByteLength = buffer.byteLength;
+                if (bufferByteLength % 2 != 0) {
+                    // NOTE(datermine): Uses to delete, now we use slice.
+                    // delete buffer[bufferByteLength - 1];
+                    buffer = buffer.slice(0, bufferByteLength - 1)
+                    
+                    // NOTE(datermine): Once we slice instead of delete, this is really
+                    // no longer necessary as it's the same as buffer.byteLength
+                    // bufferByteLength -= 1;
                 }
                 tmp.set(new Uint16Array(buffer), lastOffset);
-                lastOffset += buffer.byteLength;
+                
+                // NOTE(datermine): We could just use buffer.byteLength here.
+                lastOffset += bufferByteLength;
             });
 
             var blob = new Blob([tmp.buffer], {
